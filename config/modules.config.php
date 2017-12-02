8d46d447ee694e4136baa9a08303d750a7c7197d
Pavel <kulinovi4@yandex.ru>
2017-12-02 11:21:55 +0300

    Added zend-mvc-form component

--- config/modules.config.php
+++ config/modules.config.php
@@ -11,6 +11,11 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Zend\Form',
+    'Zend\InputFilter',
+    'Zend\Filter',
+    'Zend\Hydrator',
+    'Zend\I18n',
     'Zend\Router',
     'Zend\Validator',
 
